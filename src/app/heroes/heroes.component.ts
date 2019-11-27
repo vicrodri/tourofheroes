bb151030c82e790e6bb19fa31ab5b819e48f27c5
Vity <vicrodri@protonmail.com>
2019-11-27 12:47:41 +0100

    added mock data list and list/detail implementation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -12,10 +14,18 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Windstorm'
   };
+
+  heroes = HEROES;
+
+  selectedHero: Hero;
   
   constructor() { }
 
   ngOnInit() {
   }
 
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
