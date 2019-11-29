31fcc42563be5c383d101ed8558c943c270cee85
Vity <vicrodri@protonmail.com>
2019-11-29 10:56:55 +0100

    added search component with async http requests

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -65,6 +65,18 @@ export class HeroService {
     );
   }
   
+  /* GET heroes whose name contains search term */
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(_ => this.log(`found heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
+
   /** Log a HeroService message with the MessageService */
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
